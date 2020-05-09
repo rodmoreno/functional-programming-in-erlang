261dae85f912169e82521615a9aa2dfbc7597220
Rodrigo Moreno <rodrigo@paywithflux.com>
2020-05-08 21:29:13 -0500

    + My first erlang program 👌

---
+++ my-first-erlang-program/second.erl
@@ -0,0 +1,9 @@
+-module(second).
+
+-export([areaT/2, hypotenuse/2, perimeterT/2]).
+
+hypotenuse(A, B) -> math:sqrt(first:square(A) + first:square(B)).
+
+areaT(X, Y) -> first:mult(X, Y) / 2.
+
+perimeterT(X, Y) -> X + Y + hypotenuse(X, Y).
